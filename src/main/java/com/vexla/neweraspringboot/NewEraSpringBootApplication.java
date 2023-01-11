d2d3fc8455d69a0a7effd6db2c8b48ad1d417087
calvince <calmosh1421@gmail.com>
2023-01-11 18:04:22 +0300

    Initial Commit

---
+++ src/main/java/com/vexla/neweraspringboot/NewEraSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vexla.neweraspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewEraSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewEraSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vexla/neweraspringboot/NewEraSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vexla.neweraspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewEraSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
