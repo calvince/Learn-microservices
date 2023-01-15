ec71f655f8a0a901d6a639ae3daa3816c8d3c8bb
calvince <calmosh1421@gmail.com>
2023-01-15 23:18:07 +0300

    implement save, getEmployee and deleteEmployee by id methods

--- src/main/java/com/vexla/neweraspringboot/controller/EmployeeV2Controller.java
+++ src/main/java/com/vexla/neweraspringboot/controller/EmployeeV2Controller.java
@@ -1,16 +1,41 @@
 package com.vexla.neweraspringboot.controller;
 
 import com.vexla.neweraspringboot.model.Employee;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vexla.neweraspringboot.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v2/employees")
 public class EmployeeV2Controller {
-     @PostMapping
+    @Qualifier("employeeV2ServiceImpl")
+    @Autowired
+    private  EmployeeService employeeService;
+
+    //add qualifier so as two different version business logic to use one interface
+
+
+    @PostMapping
     public Employee save(@RequestBody Employee employee) {
-        return employee;
+        return employeeService.save(employee);
     }
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return employeeService.getAllEmployees();
+    }
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable(name = "id") String employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployeeById(@PathVariable(name = "id") String employeeId) {
+        return employeeService.deleteEmployeeById(employeeId);
+    }
+
 }
